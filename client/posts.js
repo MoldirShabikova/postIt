31a1f5c3c809c3637a997f82275ed3098f9b3b7e
moldir <moldirshb@gmail.com>
2023-01-03 08:58:48 -0600

    pot

--- client/posts.js
+++ client/posts.js
@@ -2,14 +2,35 @@ let postDisplay = document.querySelector("#display");
  
 const baseURL = `http://localhost:5000/api/posts`;
 
+const createPostCard = (posts) => {
+    const newPostCard = document.createElement('section')
+    newPostCard.innerHTML = `
+    <img src=${posts}/>
+    <p>${posts.username}</p>
+    `
+  postDisplay.appendChild(newPostCard);
+}
+
+const displayPosts = (arr) => {
+    arr.map((el) => {
+        console.log(el)
+        createPostCard(el)
+    }
+  
+)}
+
 const getAllPosts = () => {
     axios.get(baseURL)
         .then((res) => {
-            console.log(res.data)
+            displayPosts(res.data)
         })
         .catch((err) => {
         console.log(err)
     })
 }
+
+postDisplay.innerHTML = `
+
+`
 
 getAllPosts()
